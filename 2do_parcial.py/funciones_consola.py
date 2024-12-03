ec0c7e54f8c21d1b12f7df0b4ed84bcb40fe224a
Thiaguito2024 <thiagojlgd@gmail.com>
2024-12-03 18:22:55 -0300

    Candy Crush

--- 2do_parcial.py/funciones_consola.py
+++ 2do_parcial.py/funciones_consola.py
@@ -48,27 +48,16 @@ def mostrar(nombre:str, puntos:int):
 def recorrer_lista_diccionarios(lista:list, clave:str, fila:int, columna:int, direccion:int)->int:
     num_seleccionado = lista[fila][clave][columna]
     cont_iguales = 1
-    cont_vueltas = 0 
     rango = range(1, len(lista) - fila) if direccion == 1 else range(1, fila + 1)
-    # if direccion == 1: 
-        # for i in range(1, len(lista) - fila):
+
     for i in rango:
         fila_actual = fila + i if direccion == 1 else fila - i
-        # cont_vueltas += 1 
         if lista[fila_actual][clave][columna] == num_seleccionado:
             cont_iguales += 1
             if cont_iguales == 3:
                 break
         else:
-            break  
-    # elif direccion == -1: 
-    #     for i in range(1, fila+1):
-    #         if lista[fila-i][clave][columna] == num_seleccionado:
-    #             cont_iguales += 1
-    #             if cont_iguales == 3:
-    #                 break
-    #         else:
-    #             break  
+            break    
     return cont_iguales
 
 def generar_csv(nombre_archivo:str, lista:list):
