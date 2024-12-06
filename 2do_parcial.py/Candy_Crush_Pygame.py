c3f049776e8de2983700fa3a8050710f749f7161
Thiaguito2024 <thiagojlgd@gmail.com>
2024-12-06 14:40:40 -0300

    2do_parcial

--- 2do_parcial.py/Candy_Crush_Pygame.py
+++ 2do_parcial.py/Candy_Crush_Pygame.py
@@ -1,49 +1,91 @@
 import pygame
-from funciones_pygame import *
-from funciones_consola import *
-
-grilla = [
- {"piezas":[]},
- {"piezas":[]},
- {"piezas":[]},
- {"piezas":[]}]
-# Inicializa pygame y configura la pantalla
+from colores import * 
+from funciones_candy_pygame import * 
 pygame.init()
-tamaño_celda = 64
-columnas = 8
-filas = 8
-ancho_ventana = columnas * tamaño_celda
-alto_ventana = filas * tamaño_celda  
-pantalla = pygame.display.set_mode((ancho_ventana, alto_ventana))
+############################### CREO PANTALLA ###########################
+ancho_ventana = 800
+alto_ventana = 600
+pantalla = pygame.display.set_mode((ancho_ventana,alto_ventana))
 pygame.display.set_caption("Candy Crush")
-
-# Variables de el juego
-caramelo_rojo  = pygame.image.load("caramelo_rojo.png")
-caramelo_rojo = pygame.transform.scale(caramelo_rojo,(100,100))
-caramelo_azul  = pygame.image.load("caramelo_azul.png")
-caramelo_azul = pygame.transform.scale(caramelo_azul,(100,100))
-caramelo_verde  = pygame.image.load("caramelo_verde.png")
-caramelo_verde = pygame.transform.scale(caramelo_verde,(100,100))
+########################################################################
+ 
 corriendo = True
-puntaje = 0
+lista = [
+{"piezas":[]},
+{"piezas":[]},
+{"piezas":[]},
+{"piezas":[]}]
+lista_puntaje = []
+separador = ", "
+puntos = 0
+reloj = pygame.time.Clock()
+input_rect = pygame.Rect(300,400,400,75)
+play_rect = pygame.Rect(300,300,300,75)
+scoreboard_rect = pygame.Rect(10,100,250,400)
+scoreboard_inactivo = RED1
+scoreboard_activo = GREEN1
+color_input = scoreboard_inactivo
+active = False # Bandera input 
+nombre = "Ingrese su nombre: " # Texto input
+tipo_letra = pygame.font.Font(None,36) # Fuente 
+tipo_letra_scoreboard = pygame.font.Font(None,30) # Fuente scoreboard
+# imagen_fondo_general = pygame.image.load("fondo_candy_3.png")
+# imagen_fondo_general = pygame.transform.scale(imagen_fondo_general,(800,800))
 
-# Bucle principal
 while corriendo:
-    for evento in pygame.event.get():
+    lista_eventos = pygame.event.get()
+    for evento in lista_eventos:
         if evento.type == pygame.QUIT:
             corriendo = False
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            if evento.button == 1:  # Botón izquierdo del mouse
-                pos_click = evento.pos
-                manejar_click(grilla, pos_click, tamaño_celda, pantalla)
-
-    # Actualizar y mostrar la pantalla
-    pantalla.fill((0, 0, 0))  # Fondo negro
-    imprimir_carmelos(grilla, pantalla, caramelo_azul, caramelo_rojo, caramelo_verde)
-    # mostrar_puntaje(pantalla, puntaje, fuente, color, (10, 10))
+            
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(evento.pos):
+                active = True
+            else:
+                active = False
+        
+            if play_rect.collidepoint(evento.pos):
+                imagen_fondo = pygame.image.load("fondo_candy_crush_1.png")
+                pantalla.blit(imagen_fondo, (0, 0))
+                puntos = juego_principal(pantalla)
+                
+        if evento.type == pygame.KEYDOWN and active:
+                if evento.key == pygame.K_BACKSPACE: # Si presiona el DELETE, borra
+                    nombre = nombre[:-1]
+                elif evento.key == pygame.K_RETURN: #Si presiona enter, arranca el nuevo juego
+                    puntos = juego_principal(pantalla)
+                else: # Si presiona una tecla de texto, lo introduce a la cadena 
+                    nombre += evento.unicode 
+                    
+    puntaje = separador.join([nombre, str(puntos)])
+    lista_puntaje.append(puntaje)
+    
+    pantalla.fill(LIGHTBLUE)
+    # pantalla.blit(imagen_fondo_general, (100, 100))
+    color_input = scoreboard_activo if active else scoreboard_inactivo
+    pygame.draw.rect(pantalla, color_input, input_rect) 
+    pygame.draw.rect(pantalla, colores.WHEAT, play_rect)
+    pygame.draw.rect(pantalla, colores.BLACK, scoreboard_rect)
+    scoreboard_texto = tipo_letra.render("SCOREBOARD", True, colores.RED1) # Titulo de Scoreboard
+    play_texto = tipo_letra.render("PLAY",True, colores.GREEN) # Texto de boton PLAY
+    texto_renderizado = tipo_letra.render(nombre, True, (0, 0, 0)) # Texto de input
+    # Bliteo en pantalla de los textos 
+    # pantalla.blit(imagen_fondo,(0,0))
+    pantalla.blit(texto_renderizado, (input_rect.x + 10, input_rect.y + 20)) 
+    pantalla.blit(play_texto, (play_rect.x+120, play_rect.y+25))
+    pantalla.blit(scoreboard_texto, (scoreboard_rect.x+15, scoreboard_rect.y))
+    espacio = 30
+    lista_scoreboard = obtener_scoreboard() #Scoreboard
+    # Recorrido de la lista con el FOR
+    for persona in lista_scoreboard:
+        cadena_separada = persona.split(",") # Separo cada linea de la lista, con "," 
+        pantalla.blit(tipo_letra.render(f"{cadena_separada[0]}", True, colores.GREEN),(scoreboard_rect.x, scoreboard_rect.y+espacio)) # Bliteo el nombre 
+        pantalla.blit(tipo_letra.render(f"{cadena_separada[1][:-1]}", True, colores.GREEN),(scoreboard_rect.x+150, scoreboard_rect.y+espacio) ) # Bliteo el puntaje
+        espacio += 20 # Sumo 20 de espacio para que no se junten todos
+    
+    reloj.tick(30)
     pygame.display.flip()
-
-    # Control de FPS
-    pygame.time.Clock().tick(60)
-
+    
 pygame.quit()
+
+#Falta resolver puntaje y tiempo
--- 2do_parcial.py/funciones_pygame.py
+++ 2do_parcial.py/funciones_pygame.py
@@ -1,80 +1,177 @@
 import pygame
-from colores import *
 import random
+import colores
 
-def generar_grilla(lista, columnas, filas):
-    """
-    Llena una lista de diccionarios con números aleatorios entre 1 y 3.
-    """
-    for i in range(filas):
-        for j in range(columnas):
-            numero = random.randint(1, 3)
-            lista[i]["piezas"].append(numero)
-
-def imprimir_carmelos(lista,screen, caramelo_rojo,caramelo_azul,caramelo_verde): # Funcion para imprimir caramelos
-    coordenadas_fila = 150 # Donde va a estar la primera fila
-    for objeto in lista: # Por cada diccionario en la lista:
-        coordenadas_columna = 50 # Donde va a estar la primera columna
-        for caramelo in objeto["piezas"]: # Por cada caramelo en el diccionario:
-            # Dependiendo del valor (1,2,3), blitea un caramelo del color correspondiente, en las coordenadas correspondientes
-            if caramelo == 1: 
-                screen.blit(caramelo_rojo,(coordenadas_columna, coordenadas_fila))
-            elif caramelo == 2:
-                screen.blit(caramelo_azul,(coordenadas_columna, coordenadas_fila))
-            elif caramelo == 3:
-                screen.blit(caramelo_verde,(coordenadas_columna, coordenadas_fila))
-            coordenadas_columna+=100 # Con cada caramelo, se suma 100 a la coordenada, para que no esten pegados
-            if coordenadas_columna >= 700: # Cuando llega a 700, resetea
-                coordenadas_columna = 100
-        if coordenadas_fila >= 400: # Cuando llega a 400, resetea
-            coordenadas_fila = 150
-        coordenadas_fila+=100 # Con cada fila, se suma 100 a la coordenada, para que no esten pegadas
-
-def verificar_combinacion(grilla, fila, columna, clave):
-    num_seleccionado = grilla[fila][clave][columna]
-    retorno = False
-
-    if fila <= len(grilla) - 3: # Se fija si puede ir dos filas para abajo para que no se vaya de el rango
-        if (grilla[fila + 1][clave][columna] == num_seleccionado and grilla[fila + 2][clave][columna] == num_seleccionado):
-            retorno = 10
-    if fila >= 2: # Se fija si puede ir dos filas para arriba para que no se vaya de el rango
-        if (grilla[fila - 1][clave][columna] == num_seleccionado and grilla[fila - 2][clave][columna] == num_seleccionado):
-            retorno = 10
-    else: 
-        retorno = -1        
+def inicializar_matriz():
+    lista = [
+        {"piezas":[]},
+        {"piezas":[]},
+        {"piezas":[]},
+        {"piezas":[]}]
     
-    return retorno
+    return lista
 
-def manejar_click(grilla, pos_click, tamaño_celda, pantalla):
-    columna = pos_click[0] // tamaño_celda # Calculo la posicion de el click
-    fila = pos_click[1] // tamaño_celda
-
-    # Chequeo que el click este dentro de la grilla
-    if fila < len(grilla) and columna < len(grilla[fila]):
-        if verificar_combinacion(grilla, fila, columna):
-            puntaje += 10
-            nombre = input("Ingrese su nombre: ")
-
-            imprimir_carmelos
-            pygame.display.flip()
-            
+# def generar_csv(nombre_archivo:str, lista:list):
+#     """
+#     Recibe una lista y genera un archivo csv con los datos de la lista
+#     """
+#     nombre_archivo += ".csv"
+#     try:
+#         with open(nombre_archivo, "a") as archivo: 
+#             for e_tema in lista:
+#                 archivo.write(f"{e_tema}\n")
+                
+#     except FileNotFoundError:
+#         with open(nombre_archivo, "w") as archivo: 
+#             for e_tema in lista:
+#                 archivo.write(f"{e_tema}\n") 
+    
+#     return archivo
+    
 def obtener_scoreboard():
     try:
         with open("scoreboard.csv", "r+") as archivo:
             scoreboard = archivo.readlines()
+            
     except FileNotFoundError:
         with open("scoreboard.csv", "w") as archivo:
             archivo.write("Nombre, Puntos\n")
             scoreboard = []
+            
     return scoreboard
+                
+def generar_matriz(lista:list, cant_columnas:int):
+    """
+    Genera una matriz con la lista y cantidad de columnas pasadas por el usuario
+    """
+    for i in range(len(lista)):
+        for j in range(cant_columnas):
+            numero = random.randint(1,3)
+            lista[i]["piezas"].append(numero)
+            
+def verificar_combinacion(grilla, fila, columna, clave):
+    """
+    Verifica si dos filas para abajo o para arriba hay dos caramelos iguales
+    """
+    num_seleccionado = grilla[fila][clave][columna]
+    retorno = -1
 
-def agregar_y_mostrar_scoreboard(puntos): # CHEQUEAR FUNCION
-    # Obtener el contenido actual del scoreboard
-    scoreboard = obtener_scoreboard()
+    if fila <= len(grilla) - 3: # Verifica si hay espacio suficiente hacia abajo
+        if (grilla[fila + 1][clave][columna] == num_seleccionado and grilla[fila + 2][clave][columna] == num_seleccionado):
+            retorno = 10
 
-    # Pedir el nombre del jugador
-    nombre = input("Ingrese su nombre: ")
+    
+    if fila >= 2: # Verifica si hay espacio suficiente hacia arriba
+        if (grilla[fila - 1][clave][columna] == num_seleccionado and grilla[fila - 2][clave][columna] == num_seleccionado):
+            retorno = 10
 
-    # Agregar la nueva entrada al archivo
-    with open("scoreboard.csv", "a") as archivo:
-        archivo.write(f"{nombre}, {puntos}\n")
+    return retorno
+
+def juego_principal(pantalla):
+    tamaño_celda = 100
+    fila = 4
+    columna = 7
+
+    caramelo_azul = pygame.image.load("caramelo_azul.png")
+    caramelo_azul = pygame.transform.scale(caramelo_azul,(100,100))
+    caramelo_rojo = pygame.image.load("caramelo_rojo.png")
+    caramelo_rojo = pygame.transform.scale(caramelo_rojo,(100,100))
+    caramelo_verde = pygame.image.load("caramelo_verde.png")
+    caramelo_verde = pygame.transform.scale(caramelo_verde,(100,100))
+    rect_caramelos = pygame.Rect(0, 0, 400, 600)
+    
+    fuente = pygame.font.Font(None, 36)
+    puntos = 0
+    fin_del_juego = False
+    segundos_para_jugar = 11
+    cronometro = pygame.USEREVENT
+    pygame.time.set_timer(cronometro, 1000) # 1000 milisegundos equivalen a 1 segundo
+    lista = [
+        {"piezas":[]},
+        {"piezas":[]},
+        {"piezas":[]},
+        {"piezas":[]}]
+    generar_matriz(lista, 7)
+    # Bucle principal
+    while not (fin_del_juego):
+        lista_eventos = pygame.event.get() # Me guardo los eventos
+        for evento in lista_eventos:
+            if evento.type == pygame.QUIT: # Si el usuario preciona la cruz para cerrar el juego, se rompe el bucle
+                fin_del_juego = True
+                
+            if evento.type == pygame.MOUSEBUTTONDOWN: # Si el usuario hizo click
+                print("Clic detectado en posición:", evento.pos)
+                coordenada_x = int(round(evento.pos[0] / 100,0)-1)
+                coordenada_y = int(round((evento.pos[1]-100) / 100,0)-1)
+                print("Índices calculados: x =", coordenada_x, "y =", coordenada_y)
+                
+                if rect_caramelos.collidepoint(evento.pos):
+                    print("Rect caramelos")
+                    # contador_arriba = recorrer_lista_diccionarios(lista, x, "piezas", y, 1)
+                    # contador_abajo = recorrer_lista_diccionarios(lista, x, "piezas", y, -1)
+                    puntaje = verificar_combinacion(lista, coordenada_x, coordenada_y, "piezas")
+                    print(evento.pos)
+                    
+                    if puntaje == 10:
+                            print("Tiene 10 puntos ")
+                            lista = [
+                                {"piezas":[]},
+                                {"piezas":[]},
+                                {"piezas":[]},
+                                {"piezas":[]}]  
+                            generar_matriz(lista, 7)
+                            puntos += 10
+                            
+                    elif puntaje == -1:
+                            print("No tiene 10 puntos")
+                            lista = [
+                                {"piezas":[]},
+                                {"piezas":[]},
+                                {"piezas":[]},
+                                {"piezas":[]}]
+                            generar_matriz(lista, 7)
+                            segundos_para_jugar = segundos_para_jugar -1
+                            puntos += 0
+                            
+                    pygame.display.flip()            
+                                        
+        if evento.type == pygame.USEREVENT: # Si hay un evento del usuario
+                if evento.type == cronometro: # Si el userevent corresponde al cronometro
+                    if fin_del_juego == False: # Si no se llego al fin del tiempo, se resta 1 segundo
+                        segundos_para_jugar = segundos_para_jugar -1
+                        if segundos_para_jugar <= 0: 
+                            fin_del_juego = True # Pongo la bandera en true para que rompa el bucle 
+                            return puntos 
+        cronometro_texto = fuente.render(str(segundos_para_jugar), True, "green") # Texto de el cronometro
+        pantalla.blit(cronometro_texto, (100,20)) # Blit del texto de el cronometro
+        imprimir_caramelos(lista,pantalla, caramelo_rojo,caramelo_azul,caramelo_verde) 
+        pygame.display.flip() # Actualizo
+
+def imprimir_caramelos(lista, screen, caramelo_rojo, caramelo_azul, caramelo_verde):
+    """
+    Imprime caramelos en la pantalla
+    """
+    tamaño_celda = 64
+    fila = 8
+    columna = 8
+    rect_caramelos = pygame.Rect(fila, columna, tamaño_celda * fila, tamaño_celda * columna)
+    coordenadas_fila = 150 # Ubicacion de la primera fila
+    for e_dict in lista:
+        coordenadas_columna = 50 # Ubicacion de la primera columna
+        for caramelo in e_dict["piezas"]:
+            if caramelo == 1: 
+                # pygame.draw.rect(screen, colores.BLUE,rect_caramelos)
+                screen.blit(caramelo_rojo, (coordenadas_columna, coordenadas_fila), rect_caramelos)
+            elif caramelo == 2:
+                # pygame.draw.rect(screen, colores.BLUE,rect_caramelos)
+                screen.blit(caramelo_azul, (coordenadas_columna, coordenadas_fila), rect_caramelos)
+            elif caramelo == 3:
+                # pygame.draw.rect(screen, colores.BLUE,rect_caramelos)
+                screen.blit(caramelo_verde, (coordenadas_columna, coordenadas_fila), rect_caramelos)
+            
+            coordenadas_columna += 100 # Se le suma 100 a la coordenada para que no esten pegados
+            if coordenadas_columna >= 700: # Cuando llega a 700, empieza de nuevo
+                coordenadas_columna = 100
+        if coordenadas_fila >= 400: # Cuando llega a 400, empieza de nuevo
+            coordenadas_fila = 150
+        coordenadas_fila += 100 # Con cada fila, se suma 100 a la coordenada, para que no esten pegadas
